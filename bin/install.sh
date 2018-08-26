3df927fbc64ad85cd84324f4ee2ecb5cae747bd2
svein <sthorsh@gmail.com>
2018-08-26 17:22:53 +0200

    foo

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,10 @@
-# Install, configure and pull from Git
+# If alt-left|right switches tty
+# sudo reboot
+
+# Remap capslock to ctrl
+# gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier']"
+
+# Install and pull from Git
 # sudo apt-get install -y git
 # git config --global user.name svein
 # git config --global user.email sthorsh@gmail.com
@@ -7,64 +13,87 @@
 # rm ~/.bashrc
 # git pull --rebase origin master
 
-# Remap capslock to ctrl
-gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier']"
+# Install vim
+# sudo apt-get install -y vim
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Launch vim and run :PluginInstall
+  
+# Download intellij
+# ideaIC-2018.2.2-no-jdk.tar.gz from jetbrains.com
+
+# Disable gnome settings
+echo "disabling settings..."
 # Disable alt-pipe
-gsettings set org.gnome.desktop.wm.keybindings switch-group "['disabled']"
-# Disable workspace moves
+gsettings set org.gnome.desktop.wm.keybindings switch-group "['disabled']" 
+# Disable ctrl-alt-shift arrow
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['disabled']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['disabled']" 
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['disabled']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['disabled']"
 
-# Remove directories 
-if [ -d ~/Documents ]; then rm -rf ~/Documents/; fi
-if [ -d ~/Music ]; then rm -rf ~/Music/; fi
-if [ -d ~/Pictures ]; then rm -rf ~/Pictures/; fi
-if [ -d ~/Public ]; then rm -rf ~/Public/; fi
-if [ -d ~/Templates ]; then rm -rf ~/Templates/; fi
-if [ -d ~/Videos ]; then rm -rf ~/Videos/; fi
-
 # Create directories
+echo "creating directories..."
 if [ ! -e ~/bin ]; then mkdir ~/bin; fi
 if [ ! -e ~/dev ]; then mkdir ~/dev; fi
 if [ ! -e ~/doc ]; then mkdir ~/doc; fi
 if [ ! -e ~/etc ]; then mkdir ~/etc; fi
 if [ ! -e ~/install ]; then mkdir ~/install; fi
 if [ ! -e ~/tmp ]; then mkdir ~/tmp; fi
-
-# Install Ubuntu packages
+  
+# Remove directories 
+echo "removing directories..."
+if [ -d ~/Documents ]; then rm -rf ~/Documents/; fi
+if [ -d ~/Music ]; then rm -rf ~/Music/; fi
+if [ -d ~/Pictures ]; then rm -rf ~/Pictures/; fi
+if [ -d ~/Public ]; then rm -rf ~/Public/; fi
+if [ -d ~/Templates ]; then rm -rf ~/Templates/; fi
+if [ -d ~/Videos ]; then rm -rf ~/Videos/; fi
+  
+# Install Ubuntu packages: https://www.linuxquestions.org/questions/linux-software-2/dpkg-set-selections-fails-to-find-hundreds-of-packages-4175617954/
+echo "installing packages..."
+# sudo apt-get update
+sudo dselect update
 sudo dpkg --set-selections < ~/etc/dpkg-get-selections.log
+# sudo apt-get update
 sudo apt-get -u dselect-upgrade
-# 
-# Launch vim and run :PluginInstall after the clone below
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  
-# Install Java
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt update
-sudo apt install oracle-java8-set-default
-sudo ln -sf /usr/lib/jvm/java-8-oracle/ /usr/lib/jvm/java
-  
-# Install Maven
-cd ~/install
-wget http://apache.uib.no/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
-tar -xf apache-maven-3.5.4-bin.tar.gz
-sudo mv apache-maven-3.5.4 /usr/lib/
-sudo ln -sf /usr/lib/apache-maven-3.5.4/ /usr/lib/apache-maven
 
-# Install tmux
-sudo apt-get update
-wget https://github.com/tmux/tmux/releases/download/2.7/tmux-2.7.tar.gz
-tar xvzf tmux-2.7.tar.gz
-cd tmux-2.7/
-./configure && make
-sudo make install
+# # Install Java
+echo "installing java..."
+if command -v java > /dev/null 2>&1
+then
+  echo "java already installed, skipping..."
+else
+  sudo add-apt-repository ppa:webupd8team/java
+  sudo apt update
+  sudo apt install -y oracle-java8-set-default
+  sudo ln -sf /usr/lib/jvm/java-8-oracle/ /usr/lib/jvm/java
+fi
+    
+# # Install Maven
+echo "installing maven..."
+if command -v mvn > /dev/null 2>&1
+then
+  echo "maven already installed, skipping..."
+else
+  wget --directory-prefix=install http://apache.uib.no/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+  sudo tar -xf install/apache-maven-3.5.4-bin.tar.gz -C /usr/lib
+  sudo ln -sf /usr/lib/apache-maven-3.5.4/ /usr/lib/apache-maven
+fi
 
-# Install Intellij after manual download from jetbrains.com
-# cd ~/install
-# mv ~/Downloads/ideaIC-2018.2.2-no-jdk.tar.gz .
-# tar xvf ideaIC-2018.2.2-no-jdk.tar.gz
-# sudo mv idea-IC-182.4129.33/ /usr/lib/
-# sudo ln -sf /usr/lib/idea-IC-182.4129.33/ /usr/lib/idea-IC
+# Install Intellij 
+echo "installing intellij..."
+if command -v /usr/lib/idea-IC/bin/idea.sh 2>&1
+then
+  echo "intellij already installed, skipping..."
+else
+  mv ~/Downloads/ideaIC-2018.2.2-no-jdk.tar.gz install
+  sudo tar -xf install/ideaIC-2018.2.2-no-jdk.tar.gz -C /usr/lib
+  sudo ln -sf /usr/lib/idea-IC-182.4129.33/ /usr/lib/idea-IC
+fi
+  
+# All done
+echo "all done..."
+
+# Clean up favorites
+# terminal, chromium, files, settings, ...
 
