80fd07e00ec89b227be8f149b1d63c2e22115919
svein <sthorsh@gmail.com>
2018-09-03 20:50:08 +0200

    foo

--- bin/bang.sh
+++ bin/bang.sh
@@ -1,11 +1,44 @@
 # TODOs
-# hidpi 3840x2160 resolution support for squirrel see run_scaled.sh
-# hidpi 3840x2160 resolution support for sqldeveloper
+# squirrel: hidpi 3840x2160 resolution support for squirrel ref run_scaled.sh
+# sqldeveloper: hidpi 3840x2160 resolution support
 
 # Prerequisites
 # Download sqldeveloper to ~/install/
 
-# Install and pull from Git
+# Gnome
+echo "editing gnome settings..."
+# Disable alt-pipe
+gsettings set org.gnome.desktop.wm.keybindings switch-group "['disabled']" 
+# Disable ctrl-alt-shift arrow
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['disabled']" 
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['disabled']"
+# Remap capslock to ctrl
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier']"
+sleep 1
+
+# Create directories
+echo "creating directories..."
+if [ ! -e ~/bin ]; then mkdir ~/bin; fi
+if [ ! -e ~/dev ]; then mkdir ~/dev; fi
+if [ ! -e ~/doc ]; then mkdir ~/doc; fi
+if [ ! -e ~/etc ]; then mkdir ~/etc; fi
+if [ ! -e ~/install ]; then mkdir ~/install; fi
+if [ ! -e ~/tmp ]; then mkdir ~/tmp; fi
+sleep 1
+
+# Remove directories 
+echo "removing directories..."
+if [ -d ~/Documents ]; then rm -rf ~/Documents/; fi
+if [ -d ~/Music ]; then rm -rf ~/Music/; fi
+if [ -d ~/Pictures ]; then rm -rf ~/Pictures/; fi
+if [ -d ~/Public ]; then rm -rf ~/Public/; fi
+if [ -d ~/Templates ]; then rm -rf ~/Templates/; fi
+if [ -d ~/Videos ]; then rm -rf ~/Videos/; fi
+sleep 1
+
+# Git
 git --version 2>&1 >/dev/null
 if [ ! $? -eq 0 ]
 then
@@ -24,8 +57,9 @@ then
 else
   echo "git already installed, skipping..."
 fi
+sleep 1
 
-# Install vim
+# Vim
 vim --version 2>&1 >/dev/null
 if [ ! $? -eq 0 ]
 then
@@ -36,68 +70,60 @@ then
 else
   echo "vim already installed, skipping..."
 fi
-
-# Edit gnome settings
-echo "editing gnome settings..."
-# Remap capslock to ctrl
-gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier']"
-# Disable alt-pipe
-gsettings set org.gnome.desktop.wm.keybindings switch-group "['disabled']" 
-# Disable ctrl-alt-shift arrow
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['disabled']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['disabled']" 
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['disabled']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['disabled']"
-
-# Create directories
-echo "creating directories..."
-if [ ! -e ~/bin ]; then mkdir ~/bin; fi
-if [ ! -e ~/dev ]; then mkdir ~/dev; fi
-if [ ! -e ~/doc ]; then mkdir ~/doc; fi
-if [ ! -e ~/etc ]; then mkdir ~/etc; fi
-if [ ! -e ~/install ]; then mkdir ~/install; fi
-if [ ! -e ~/tmp ]; then mkdir ~/tmp; fi
-
-# Remove directories 
-echo "removing directories..."
-if [ -d ~/Documents ]; then rm -rf ~/Documents/; fi
-if [ -d ~/Music ]; then rm -rf ~/Music/; fi
-if [ -d ~/Pictures ]; then rm -rf ~/Pictures/; fi
-if [ -d ~/Public ]; then rm -rf ~/Public/; fi
-if [ -d ~/Templates ]; then rm -rf ~/Templates/; fi
-if [ -d ~/Videos ]; then rm -rf ~/Videos/; fi
-
-# Install Ubuntu packages
-echo "installing packages..."
-sudo apt-get update
-sudo apt-get install -y dselect
-sudo dselect update
-sudo dpkg --set-selections < ~/etc/dpkg-get-selections.log
-sudo apt-get -u dselect-upgrade
-
-# Install chrome
-echo "installing chrome..."
-sudo apt-get install -y chromium-browser
 sleep 1
 
-# Install slack
-echo "installing slack..."
+# Tmux
+if [ -d ~/.tmux/plugins/tpm ]
+then
+  echo "tmux tpm already cloned, skipping..."
+else
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+sleep 1
+
+# Ubuntu packages
+sudo apt-get update
+if command -v chromium-browser > /dev/null 2>&1
+then
+  echo "apt update and upgrade..."
+  sudo apt-get -u dist-upgrade
+else
+  echo "apt update and install..."
+  sudo apt-get install -y dselect
+  sudo dselect update
+  sudo dpkg --set-selections < ~/etc/dpkg-get-selections.log
+  sudo apt-get -u dselect-upgrade
+fi
+sleep 1
+
+# Chrome
+if command -v chromium-browser > /dev/null 2>&1
+then
+  echo "chrome already installed, skipping..."
+else
+  echo "installing chrome..."
+  sudo apt-get install -y chromium-browser
+fi
+sleep 1
+
+# Slack
 if command -v slack > /dev/null 2>&1
 then
   echo "slack already installed, skipping..."
 else
+  echo "installing slack..."
   wget --directory-prefix=install "https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.1-amd64.deb"
   sudo apt-get install gdebi-core -y
   sudo gdebi install/slack-desktop-3.3.1-amd64.deb
 fi
 sleep 1
 
-# Install Java
-echo "installing java..."
+# Java
 if command -v java > /dev/null 2>&1
 then
   echo "java already installed, skipping..."
 else
+  echo "installing java..."
   sudo add-apt-repository ppa:webupd8team/java
   sudo apt-get update
   sudo apt-get install -y oracle-java8-set-default
@@ -105,36 +131,36 @@ else
 fi
 sleep 1
 
-# Install Maven
-echo "installing maven..."
+# Maven
 if command -v mvn > /dev/null 2>&1
 then
   echo "maven already installed, skipping..."
 else
+  echo "installing maven..."
   wget --directory-prefix=install http://apache.uib.no/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
   sudo tar -xf install/apache-maven-3.5.4-bin.tar.gz -C /usr/lib
   sudo ln -sf /usr/lib/apache-maven-3.5.4/ /usr/lib/apache-maven
 fi
 sleep 1
 
-# Install Intellij
-echo "installing intellij..."
-if command -v /usr/lib/idea-IC/bin/idea.sh 2>&1
+# Intellij
+if command -v /usr/lib/idea-IC/bin/idea.sh > /dev/null 2>&1
 then
   echo "intellij already installed, skipping..."
 else
+  echo "installing intellij..."
   wget --directory-prefix=install https://download.jetbrains.com/idea/ideaIC-2018.2.2-no-jdk.tar.gz
   sudo tar -xf install/ideaIC-2018.2.2-no-jdk.tar.gz -C /usr/lib
   sudo ln -sf /usr/lib/idea-IC-182.4129.33/ /usr/lib/idea-IC
 fi
 sleep 1
 
-# Install sqldeveloper
-echo "installing sqldeveloper..."
+# SQLdeveloper
 if [ -f /usr/lib/sqldeveloper/sqldeveloper.sh ]
 then
   echo "sqldeveloper already installed, skipping..."
 else
+  echo "installing sqldeveloper..."
   if  [ -d /usr/lib/sqldeveloper/ ]
   then 
     sudo rm -rf /usr/lib/sqldeveloper/
@@ -143,12 +169,12 @@ else
 fi
 sleep 1
 
-# Install squirrel
-echo "installing squirrel..."
+# Squirrel
 if [ -f /usr/lib/squirrelsql/squirrel-sql.sh ]
 then
   echo "squirrel already installed, skipping..."
 else
+  echo "installing squirrel..."
   sudo rm -rf /usr/lib/squirrelsql*
   wget --directory-prefix=install https://sourceforge.net/projects/squirrel-sql/files/1-stable/3.8.1-plainzip/squirrelsql-3.8.1-standard.zip
   sudo unzip install/squirrelsql-3.8.1-standard.zip -d /usr/lib > /dev/null
@@ -157,28 +183,18 @@ else
 fi
 sleep 1
 
-# Clone tpm from git
-if [ -d ~/.tmux/plugins/tpm ]
-then
-  echo "tmux tpm already cloned, skipping..."
-else
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
 
 # All done
 echo "all done..."
-
-# If alt-left|right switches tty
-# sudo reboot
-
-# Patch packages
-# Use software updater
 
 # Git
 # Add ssh key to github
 
 # Vim
 # Launch and run :PluginInstall
+
+# Patch packages
+# Use software updater
 
 # Background
 # ubuntu-logo.jpg
@@ -191,4 +207,7 @@ echo "all done..."
 # guake
 # gnome terminal
 # SSH Key Agent: /usr/bin/gnome-keyring-daemon --start --components=ssh (GNOME Keyring: SSH Agent)
+
+# If alt-left|right switches tty
+# sudo reboot
 
